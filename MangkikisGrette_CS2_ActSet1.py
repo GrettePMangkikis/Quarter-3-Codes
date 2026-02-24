43b5a3ce0ec60060184d54386e08fd71b5c0d49f
Grette Polo Mangkikis <grettepolo.mangkikis@smc.pshs.edu.ph>
2026-02-24 19:46:25 +0800

    Add files via upload

---
+++ MangkikisGrette_CS2_ActSet1.py
@@ -0,0 +1,10 @@
+student = {}
+
+student['name'] = input("Enter your name: ")
+student['age'] = input("Enter your age: ")
+student['subject'] = input("Enter your favourite subject: ")
+
+print('\n Student Record:')
+print('\n Name:', student['name'])
+print('\n Age:', student['age'])
+print("\n Favourite subject:", student['subject'])
